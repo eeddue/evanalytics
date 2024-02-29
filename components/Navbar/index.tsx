13602ffbdc3f47434acb269b4c229d2935ec1a9d
eeddue <mainaedwin201@gmail.com>
2024-02-29 16:42:57 +0300

    sidebar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Button } from "../ui/button";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 const NavItems = [
   { name: "NFL", href: "/nfl" },
@@ -18,7 +19,7 @@ function Navbar() {
         <div className="flex space-x-7 items-center">
           <Image src="/images/logo.webp" width={100} height={100} alt="logo" />
 
-          <ul className="flex gap-5">
+          <ul className="md:flex gap-5 hidden">
             {NavItems.map((item) => (
               <Link href="/" key={item.href}>
                 {item.name}
@@ -31,6 +32,17 @@ function Navbar() {
           <Link href="/">Products</Link>
           <Link href="/">Tools</Link>
           <button>Login</button>
+
+          <Sheet>
+            <SheetTrigger className="md:hidden">
+              <MenuIcon />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col gap-4">
+              {[...NavItems, { href: "/products", name: "Products" }, { href: "/tools", name: "Tools" }].map((item) => (
+                <p key={item.href}>{item.name}</p>
+              ))}
+            </SheetContent>
+          </Sheet>
         </div>
       </header>
     </nav>
