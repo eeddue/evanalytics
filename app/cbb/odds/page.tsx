3e0884a7232c5837b69ea81d23415b45f2c7dd1f
eeddue <mainaedwin201@gmail.com>
2024-03-10 12:22:56 +0300

    more pages

--- app/cbb/odds/page.tsx
+++ app/cbb/odds/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function CbbOdds() {
-  return <div className="pagew mx-auto">CbbOdds</div>;
+  return (
+    <div className="pagew mx-auto">
+      <section className="p-3 border-b-2 border-border bg-muted flex justify-center w-full">
+        <p className="text-lg">College Basketball Odds and Live Movement</p>
+      </section>
+    </div>
+  );
 }
 
 export default CbbOdds;
--- app/cfb/odds/page.tsx
+++ app/cfb/odds/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function CfbOdds() {
-  return <div className="pagew mx-auto">CfbOdds</div>;
+  const { data: events } = useQuery({
+    queryKey: ["cfbEvents"],
+    queryFn: () => {
+      return [];
+    },
+  });
+
+  return (
+    <div className="pagew mx-auto">
+      <section className="p-3 border-b-2 border-border bg-muted flex justify-center w-full">
+        <p className="text-lg">College Football Odds and Live Movement</p>
+      </section>
+    </div>
+  );
 }
 
 export default CfbOdds;
